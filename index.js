047eebb521f99f633ca4100973fa5510a7bb90b8
ameyBhojane <abrock@gmail.com>
2024-02-23 22:37:31 +0530

    index updated

--- index.js
+++ index.js
@@ -1,89 +1,19 @@
-#!/usr/bin/env node
+const express = require('express')
+const app = express()
+const port = 9000
+const cors = require('cors')
 
-/**
- * Module dependencies.
- */
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+let transactionsRouter = require('./routes/transactions');
 
-var app = require('./app');
-var debug = require('debug')('server:server');
-var http = require('http');
-/**
- * Get port from environment and store in Express.
- */
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use(cors());
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+app.use('/users', usersRouter);
+app.use('/transactions', transactionsRouter);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
