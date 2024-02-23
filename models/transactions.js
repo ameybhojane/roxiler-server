d4620d585e3945586d0a6c8c5a9c7c3f761724eb
ameyBhojane <abrock@gmail.com>
2024-02-23 22:59:33 +0530

    dialect module

--- models/transactions.js
+++ models/transactions.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize('sql6685510', 'sql6685510', '5Hij3QPwjF', {
     host: 'sql6.freesqldatabase.com',
-    dialect: 'mysql2'
+    dialect: 'mysql',
+    dialectModule: 'mysql2'
 });
 const { DataTypes } = require('sequelize');
 
--- routes/transactions.js
+++ routes/transactions.js
@@ -7,7 +7,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize('sql6685510', 'sql6685510', '5Hij3QPwjF', {
     host: 'sql6.freesqldatabase.com',
-    dialect: 'mysql2'
+    dialect: 'mysql',
+    dialectModule: 'mysql2'
 });
 const { QueryTypes } = require('sequelize');
 
