1826eebb538863e14cf2aff0a7dc2bda941bfeb5
ameyBhojane <abrock@gmail.com>
2024-02-23 22:42:23 +0530

    cors removed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 9000
-const cors = require('cors')
+// const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,7 +10,7 @@ let transactionsRouter = require('./routes/transactions');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.use(cors());
+// app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/transactions', transactionsRouter);
